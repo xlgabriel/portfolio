fe68cac992f97a7329726ce5f4e8474eb404124f
xlgabriel <gabriel.jeannot@uao.edu.co>
2023-06-17 23:58:27 -0500

    New main name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Gabriel</span>
+            Hi, I'm <span className='text-[#915EFF]'>Gabriel Jeannot</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Multimedia Engineer | A.I. Specialization student<br className='sm:block hidden' />
