d02bab0ad72e7beb43d33dcf552553efc7ac09d3
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-01-25 11:48:31 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log('VITE_APP_EMAILJS_SERVICE_ID key is available:', Boolean(process.env.VITE_APP_EMAILJS_SERVICE_ID));
+console.log('VITE_APP_EMAILJS_TEMPLATE_ID key is available:', Boolean(process.env.VITE_APP_EMAILJS_TEMPLATE_ID));
+console.log('VITE_APP_EMAILJS_PUBLIC_KEY key is available:', Boolean(process.env.VITE_APP_EMAILJS_PUBLIC_KEY));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
