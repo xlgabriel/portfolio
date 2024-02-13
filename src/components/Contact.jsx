cf7cd52f475dc25856cf6c0cdaa7891f07d775c0
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-02-13 14:46:05 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -111,7 +111,7 @@ const Contact = () => {
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your awesome name?"
               className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ${errors.name ? "border-red-500" : ""
                 }`}
             />
@@ -128,7 +128,7 @@ const Contact = () => {
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ${errors.email ? "border-red-500" : ""
                 }`}
             />
