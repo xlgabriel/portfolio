881c8048a15a2ca7f275a7f932e9e05df12aaa9c
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-02-27 13:56:02 -0500

    Buttons text updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,10 +30,10 @@ const Hero = () => {
           </p>
           <div className="flex sm:flex-row mt-7">
             <a href="https://github.com/xlgabriel" target="_blank" rel="noopener noreferrer" className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card">
-              <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">My GitHub</button>
+              <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">GitHub</button>
             </a>
             <a href="https://www.linkedin.com/in/xlgabriel" target="_blank" rel="noopener noreferrer" className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card">
-              <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">My LinkedIn</button>
+              <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">LinkedIn</button>
             </a>
             <a href="mailto:gabriel.jeannot@uao.edu.co" target="_blank" rel="noopener noreferrer" className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card">
               <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">Email me</button>
