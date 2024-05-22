5c9515ef19a2756e8fb0acb701a16d4813d7d702
Gabriel Jeannot <gabriel.jeannot@uao.edu.co>
2024-05-22 13:10:53 -0400

    fix: main branch now deploys portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, LinkTree } from "./components";
 
-const MainContent = () => {
-    return (
-        <>
-            <About />
-            <Works />
-            <Experience />
-            {/* <Tech /> */}
-            <Feedbacks />
-            <div className="relative z-0">
-                <Contact />
-                <StarsCanvas />
-            </div>
-        </>
-    );
-};
-
-const AppContent = () => {
-    return (
-        <div className="relative z-0 bg-primary">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-                                <Navbar usedOnLinkTree={false} />
-                                <Hero />
-                            </div>
-                            <MainContent />
-                        </>
-                    }
-                />
-                <Route
-                    path="/links"
-                    element={
-                        <>
-                            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center min-h-screen">
-                                <Navbar usedOnLinkTree={true} />
-                                <LinkTree />
-                            </div>
-                            <Contact />
-                        </>
-                    }
-                />
-            </Routes>
-        </div>
-    );
-};
-
 const App = () => {
     return (
-        <BrowserRouter basename="/portfolio">
-            <AppContent />
+        <BrowserRouter>
+            <div className="relative z-0 bg-primary">
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Works />
+                <Experience />
+                {/* <Tech /> */}
+                <Feedbacks />
+                <div className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
+            </div>
         </BrowserRouter>
     );
 };
