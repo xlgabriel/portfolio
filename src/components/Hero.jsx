7b2b9dbae421ac06d7226f77726ab42aeadf67b2
Gabriel Jeannot <70666172+xlgabriel@users.noreply.github.com>
2024-12-25 13:55:48 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
                             </button>
                         </a>
                         <a
-                            href="https://drive.google.com/file/d/1wNSo6dtOsfWeCJu854kqBztZDKI_ZjBZ/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1-mN7_PWG_7LK0iTuAJEwI2GMgtWKN6Bq/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
